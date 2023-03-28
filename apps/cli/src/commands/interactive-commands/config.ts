d36986129e2782670074d537de42ac2a16658582
Tomas Reimers <tomas@tomasreimers.com>
2023-03-28 16:13:03 -0400

    feat: config command (#1368)
    * cleanup: remove preferred submit command
    
    * feat: config command

---
+++ apps/cli/src/commands/interactive-commands/config.ts
@@ -0,0 +1,25 @@
+import yargs from 'yargs';
+import { graphite } from '../../lib/runner';
+
+const args = {
+  config: {
+    demandOption: false,
+    type: 'string',
+    positional: true,
+    describe: 'The config to load.',
+  },
+} as const;
+
+export const command = 'config [config]';
+export const canonical = 'interactive config';
+export const description = false;
+export const builder = args;
+
+type argsT = yargs.Arguments<yargs.InferredOptionTypes<typeof args>>;
+export const handler = async (argv: argsT): Promise<void> => {
+  return graphite(argv, canonical, async (context) => {
+    void context;
+
+    throw new Error('Unimplemented');
+  });
+};
