fc15f54227654d5ddd3db2a52cd424c26c02939e
Jacob Gold <jacob@graphite.dev>
2023-03-30 18:19:50 -0400

    feat: amend (#1390)
    * feat: uncommit
    
    * fix: status bug
    
    * feat: revert
    
    * feat: commit
    
    * feat: amend

--- apps/gti/src/operations/AmendOperation.ts
+++ apps/gti/src/operations/AmendOperation.ts
@@ -6,7 +6,7 @@ import type {
   PreviewContext,
   UncommittedChangesPreviewContext,
 } from "../previews";
-import type { CommandArg, UncommittedChanges } from "../types";
+import type { UncommittedChanges } from "../types";
 
 import { Operation } from "./Operation";
 
@@ -16,6 +16,9 @@ export class AmendOperation extends Operation {
    * @param message if provided, update commit description to use this title & description
    */
   constructor(
+    /**
+     * unused
+     */
     private filePathsToAmend?: Array<RepoRelativePath>,
     private message?: EditedMessage
   ) {
@@ -25,25 +28,7 @@ export class AmendOperation extends Operation {
   static opName = "Amend";
 
   getArgs() {
-    const args: Array<CommandArg> = ["amend"];
-    if (this.filePathsToAmend) {
-      args.push(
-        ...this.filePathsToAmend.map((file) =>
-          // tag file arguments specialy so the remote repo can convert them to the proper cwd-relative format.
-          ({
-            type: "repo-relative-file" as const,
-            path: file,
-          })
-        )
-      );
-    }
-    if (this.message) {
-      args.push(
-        "--message",
-        `${this.message.title}\n${this.message.description}`
-      );
-    }
-    return args;
+    return ["commit", "amend", "-n"];
   }
 
   makeOptimisticUncommittedChangesApplier?(
