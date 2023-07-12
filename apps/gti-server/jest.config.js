fb9d171cba9a58d91ad5a8c92ca336ff11ab9381
Jacob Gold <jacob@graphite.dev>
2023-07-12 14:38:23 -0400

    fix

--- apps/gti-server/jest.config.js
+++
@@ -1,9 +0,0 @@
-
-
-/* global module */
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: ['<rootDir>/src/setupTests.ts'],
-};
--- apps/vscode/jest.config.js
+++
@@ -1,7 +0,0 @@
-
-/* global module */
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
